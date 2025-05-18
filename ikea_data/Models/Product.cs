10f38b0f9eac90a7484c7b59672ecde6e3b04fad
arotrar55 <arotar055@gmail.com>
2025-05-18 20:43:16 +0200

    last update merge

--- ikea_data/Models/Product.cs
+++ ikea_data/Models/Product.cs
@@ -8,7 +8,7 @@ public class Product
     public string  Name             { get; set; } = "";
     public decimal Price            { get; set; }
     public string? MainImage        { get; set; }
-    public string?  Description     { get; set; }  
+    public string? Description     { get; set; }  
     public string? Color            { get; set; }
     public string? Dimensions       { get; set; }  
     public decimal? Weight          { get; set; }   
